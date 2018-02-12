a6aba1d6ab0a2a840427c2539c89fa6038f5fc85
Martynas Žilinskas <martiogalaltu@gmail.com>
2018-02-12 13:18:42 +0200

    Added debugging.

---
+++ test/test-runner.ts
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2016 Palantir Technologies, Inc.
+ * Original source: https://goo.gl/U1JdRW
+ */
+
+import * as glob from "fast-glob";
+import * as path from "path";
+import { consoleTestResultHandler, runTest } from "tslint/lib/test";
+
+process.stdout.write("\nTesting Lint Rules:\n");
+
+const testDirectories = glob.sync("./rules/**/tslint.json").map(path.dirname);
+const rulesDirectory = path.resolve(process.cwd(), "../rules");
+
+for (const testDirectory of testDirectories) {
+    const results = runTest(testDirectory, rulesDirectory);
+    const didAllTestsPass = consoleTestResultHandler(results, {
+        log(m): void {
+            process.stdout.write(m);
+        },
+        error(m): void {
+            process.stderr.write(m);
+        }
+    });
+    if (!didAllTestsPass) {
+        process.exitCode = 1;
+        break;
+    }
+}
